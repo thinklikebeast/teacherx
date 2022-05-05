42fccf00f4bd1aa10be5fb8cbfdd7f8da9428408
dagim_sole <81900860+thinklikebeast@users.noreply.github.com>
2022-05-05 08:04:31 +0300

    Add files via upload

---
+++ src/components/task-item.tsx
@@ -0,0 +1,106 @@
+import React, { useCallback } from 'react'
+import { PanGestureHandlerProps } from 'react-native-gesture-handler'
+import { NativeSyntheticEvent, TextInputChangeEventData } from 'react-native'
+import {
+  Pressable,
+  Box,
+  HStack,
+  useColorModeValue,
+  Icon,
+  Input,
+  useToken
+} from 'native-base'
+import AnimatedCheckbox from 'react-native-checkbox-reanimated'
+import AnimatedTaskLabel from './animated-task-label'
+import { Feather } from '@expo/vector-icons'
+import {View} from 'react-native'
+interface Props extends Pick<PanGestureHandlerProps, 'simultaneousHandlers'> {
+  isEditing: boolean
+  isDone: boolean
+  onToggleCheckbox?: () => void
+  onPressLabel?: () => void
+  onRemove?: () => void
+  onChangeSubject?: (subject: string) => void
+  onFinishEditing?: () => void
+  subject: string
+}
+
+const TaskItem = (props: Props) => {
+  const {
+    isEditing,
+    isDone,
+    onToggleCheckbox,
+    subject,
+    onPressLabel,
+    onRemove,
+    onChangeSubject,
+    onFinishEditing,
+    simultaneousHandlers
+  } = props
+
+  const highlightColor = useToken(
+    'colors',
+    useColorModeValue('blue.500', 'blue.400')
+  )
+  const boxStroke = useToken(
+    'colors',
+    useColorModeValue('muted.300', 'muted.500')
+  )
+
+  const checkmarkColor = useToken('colors', useColorModeValue('white', 'white'))
+
+  const activeTextColor = useToken(
+    'colors',
+    useColorModeValue('darkText', 'lightText')
+  )
+  const doneTextColor = useToken(
+    'colors',
+    useColorModeValue('muted.400', 'muted.600')
+  )
+
+  const handleChangeSubject = useCallback(
+    (e: NativeSyntheticEvent<TextInputChangeEventData>) => {
+      onChangeSubject && onChangeSubject(e.nativeEvent.text)
+    },
+    [onChangeSubject]
+  )
+
+  return (
+    <View
+    >
+      <HStack
+        alignItems="center"
+        w="full"
+        px={4}
+        py={2}
+        bg={useColorModeValue('warmGray.50', 'primary.900')}
+      > 
+        {isEditing ? (
+          <Input
+            placeholder="Studnet name"
+            value={subject}
+            variant="unstyled"
+            fontSize={19}
+            px={1}
+            py={0}
+            autoFocus
+            blurOnSubmit
+            onChange={handleChangeSubject}
+            onBlur={onFinishEditing}
+          />
+        ) : (
+          <AnimatedTaskLabel
+            textColor={activeTextColor}
+            inactiveTextColor={doneTextColor}
+            strikethrough={isDone}
+            onPress={onPressLabel}
+          >
+            {subject}
+          </AnimatedTaskLabel>
+        )}
+      </HStack>
+    </View>
+  )
+}
+
+export default TaskItem
